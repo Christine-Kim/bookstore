7dd477ca4845671de449f7ad9e16ff68e53a280d
leland_kim <kim.l.christine@gmail.com>
2014-09-04 20:55:01 -0400

    Display books from database

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,6 +1,8 @@
 class BooksController < ApplicationController
 def index
-	@available_at = Time.now
-@books = ["Atlas Shrugged", "Ender's Game", "Hamlet", "The Hobbit"]
+#@available_at = Time.now
+#@books = ["Atlas Shrugged", "Ender's Game", "Hamlet", "The Hobbit"]
+@available_at = Time.now
+@books = Book.all
 end
 end
