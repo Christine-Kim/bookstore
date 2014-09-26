d014c4be852929de7b53fdde2931643447736cb2
leland_kim <kim.l.christine@gmail.com>
2014-09-25 20:55:59 -0400

    added books index page

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,12 +1,13 @@
 class BooksController < ApplicationController
 def index
+
+
 @available_at = Time.now
 #@books = ["Atlas Shrugged", "Ender's Game", "Hamlet", "The Hobbit"]
-@books = Book.all
-#end
-def show
-#@book = Book.find(params[:id])
 
+@books = Book.all
 end
 end
-end
+#end
+#def show
+#@book = Book.find(params[:id])
